166fd6ca674e5d7e0a787b47718a8f0c0fcc04be
Plode <84858705+plode-v@users.noreply.github.com>
2023-05-21 23:41:42 +0700

    Change download pictures

---
+++ client/src/assets/index.js
@@ -0,0 +1,3 @@
+export { default as iphone } from "./iphone.png"
+export { default as appleStore } from "./apple-store.png"
+export { default as googleStore } from "./google-store.png"
--- client/src/pages/HeroBanner.jsx
+++ client/src/pages/HeroBanner.jsx
@@ -1,4 +1,4 @@
-import iphone from "../assets/Free_Iphone_14_Pro_Mockup_3.png"
+import { iphone, appleStore, googleStore } from "../assets"
 
 const HeroBanner = () => {
     return (
@@ -8,8 +8,8 @@ const HeroBanner = () => {
                     <h1 className="text-[70px] leading-none md:text-[100px] uppercase font-[700] tracking-tight">Fit made easy</h1>
                     <h4 className="text-[26px] md:text-[32px] font-[300] w-[90%]">Start healthy habits now with EasyFit</h4>
                     <div className="flex items-start gap-1 md:gap-4 md:py-10 py-5 justify-center sm:justify-start lg:flex-row flex-col">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Download_on_the_App_Store_Badge.svg" alt="Apple Store" className="h-[50px] lg:h-[60px] aspect-[3.375/1] hover:cursor-pointer" />
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Google_Play_2022_logo.svg" alt="Google Playstore" className=" border border-black p-2 rounded-md h-[50px] lg:h-[60px] aspect-[3.375/1] bg-white hover:cursor-pointer"  />
+                        <img src={appleStore} alt="Apple Store" className=" hover:cursor-pointer h-[50px] lg:h-[60px]" />
+                        <img src={googleStore} alt="Google Playstore" className="h-[50px] lg:h-[60px] aspect-[3.375/1] hover:cursor-pointer"  />
                     </div>
                 </div>
                 <div className="flex-1 hidden lg:flex justify-end items-center mt-[350px] pr-10">
