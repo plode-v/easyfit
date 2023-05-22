1e45d54b09265cf3ea316bcdfc1bce24595b5efd
Plode <84858705+plode-v@users.noreply.github.com>
2023-05-22 19:02:37 +0700

    Update carousels

--- client/src/pages/Proposals.jsx
+++ client/src/pages/Proposals.jsx
@@ -5,13 +5,15 @@ const Proposals = () => {
     return (
         <div className="xl:h-[700px] h-max bg-white z-10 flex justify-center items-center">
             <Carousel className="xl:w-[1000px] w-3/4 py-20">
-                <Carousel.Item>
+                {/* Fix this and make it a carousel mapping */}
+                {/* Move images into constants folder */}
+                <Carousel.Item className="border-black border-[2px]">
                     <img
                         src={screenShot}
                         className="w-full"
                     />
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item className="border-black border-[2px]">
                     <img 
                         src={screenShot} 
                         className="w-full"
