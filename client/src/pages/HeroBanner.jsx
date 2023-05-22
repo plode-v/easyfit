5d0bc9a41e54fa3e90a305db832a9b83e35280eb
Plode <84858705+plode-v@users.noreply.github.com>
2023-05-22 17:21:51 +0700

    Update Hero banner

--- client/src/pages/HeroBanner.jsx
+++ client/src/pages/HeroBanner.jsx
@@ -2,7 +2,7 @@ import { iphone, appleStore, googleStore } from "../assets"
 
 const HeroBanner = () => {
     return (
-        <div className="flex h-max xl:h-[60vh] lg:h-[65vh] bg-green-500 pt-[60px] items-center justify-center z-10">
+        <div className="flex h-max xl:h-[640px] lg:h-[65vh] bg-green-500 pt-[60px] items-center justify-center z-10">
             <div className="flex w-full lg:w-[1300px] justify-center items-center flex-col md:flex-row">
                 <div className="flex-1 flex flex-col w-full h-max text-white md:pl-10 pl-2 py-10">
                     <h1 className="text-[70px] leading-none md:text-[100px] uppercase font-[700] tracking-tight">Fit made easy</h1>
@@ -13,7 +13,7 @@ const HeroBanner = () => {
                     </div>
                 </div>
                 <div className="flex-1 relative hidden md:flex justify-end items-center xl:mt-[240px] xl:mr-10 mr-24">
-                    <img src={iphone} alt="iphone" className="xl:h-[750px] lg:h-[550px] h-[500px] hover:-translate-y-[30px] duration-200" />
+                    <img src={iphone} alt="iphone" className="xl:h-[700px] lg:h-[550px] h-[500px] hover:-translate-y-[30px] duration-200" />
                 </div>
             </div>
         </div>
