b6988864d380ae5aa28267d28f6321f7fd8a3694
Plode <84858705+plode-v@users.noreply.github.com>
2023-06-03 00:23:20 +0700

    Deployed onto AWS EC2 server

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://54.177.106.115:3000',
         changeOrigin: true,
         secure: false
       }
