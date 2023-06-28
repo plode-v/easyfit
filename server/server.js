5ca0537c86fc74255a8c4e6564b6556d5bbb4b0f
Plode-v <plodevanichaka@gmail.com>
2023-06-28 17:44:28 +0700

    Update cors

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT
 
 const corsOptions = {
-    origin: "http://localhost:5173"
+    origin: "https://easyfit-sepia.vercel.app"
 };
 
 app.use(express.json())
