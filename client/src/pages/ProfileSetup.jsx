b21f142a98182faa6139bd6b00c405deb5aca893
Plode <84858705+plode-v@users.noreply.github.com>
2023-07-11 13:53:15 +0700

    Update profile page

--- client/src/pages/ProfileSetup.jsx
+++ client/src/pages/ProfileSetup.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from 'react-router-dom';
 import { apiKey } from '../constants';
 
 const ProfileSetup = () => {
-    // TODO: make this page popup right when user register.
 
     const { dispatch } = useProfileContext();
     const { user } = useAuthContext();
